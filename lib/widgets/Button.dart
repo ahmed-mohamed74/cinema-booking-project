994e7f2d665d9fb1caa76ddf3f63b148ea47e4e5
badour <ahmedmohamed152002@gmail.com>
2023-02-14 19:06:35 +0200

    toggle_categories

--- lib/widgets/Button.dart
+++ lib/widgets/Button.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
-Widget Btn(String name) {
-  return ElevatedButton
-    (
-    onPressed: null,
-    child: Text(name,style: TextStyle(color: Colors.black),),style: ButtonStyle(
+Widget Btn(String name, Function() function) {
+  return ElevatedButton(
+    onPressed: function,
+    child: Text(
+      name,
+      style: TextStyle(color: Colors.white),
+    ),
+    style: ButtonStyle(
         shape: MaterialStateProperty.all<RoundedRectangleBorder>(
             RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(18.0),
-                side: BorderSide(color: Colors.grey)
-            )
-        )
-    ),);
+                side: BorderSide(color: Colors.white)))),
+  );
 }
